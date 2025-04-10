a4399639325f46352191c1f6dfefae20cfda8003
yurikuit <89903043+yurikuit@users.noreply.github.com>
2025-04-10 12:38:24 +0200

    Update 970-arch.sh
    bug fix

--- Personal/970-arch.sh
+++ Personal/970-arch.sh
@@ -4,34 +4,30 @@
 
 installed_dir=$(dirname $(readlink -f $(basename `pwd`)))
 
-if [ -f /usr/local/bin/get-nemesis-on-arch ]; then
+echo
+tput setaf 2
+echo "################################################################"
+echo "################### We are on an ARCH LINUX BASED iso"
+echo "################################################################"
+tput sgr0
+echo
 
+echo
+echo "Copying /etc/environment"
+echo
+sudo cp -vf $installed_dir/settings/environment/environment /etc/environment
+echo
+
+if [ -f /usr/share/xsessions/xfce.desktop ]; then
 	echo
 	tput setaf 2
 	echo "################################################################"
-	echo "################### We are on an ARCH LINUX BASED iso"
+	echo "################### We are on Xfce4"
 	echo "################################################################"
 	tput sgr0
 	echo
-
+	echo "Changing the whiskermenu"
 	echo
-	echo "Copying /etc/environment"
-	echo
- 	sudo cp -vf $installed_dir/settings/environment/environment /etc/environment
-	echo
-
-	if [ -f /usr/share/xsessions/xfce.desktop ]; then
-		echo
-		tput setaf 2
-		echo "################################################################"
-		echo "################### We are on Xfce4"
-		echo "################################################################"
-		tput sgr0
-		echo
-		echo "Changing the whiskermenu"
-		echo
-		cp $installed_dir/settings/archlinux/whiskermenu-7.rc ~/.config/xfce4/panel/whiskermenu-7.rc
-
-	fi	
+	cp $installed_dir/settings/archlinux/whiskermenu-7.rc ~/.config/xfce4/panel/whiskermenu-7.rc
 
 fi
